20707a8604c2e710d5e2263a58427f6f98d38a3d
theguardsgod <1043843535@qq.com>
2023-12-11 03:42:53 +0800

    first commit

--- scripts/Exp_000.sh
+++ scripts/Exp_000.sh
@@ -8,10 +8,10 @@ experiment_index=${file_name##*_}
 experiment_index=${experiment_index%%.*}
 
 
-CUDA_VISIBLE_DEVICES=2 python -u src/trainer.py \
+CUDA_VISIBLE_DEVICES=0 python -u src/trainer.py \
     --experiment_index=$experiment_index \
     --cudas=0 \
-    --n_epochs=2000 \
+    --n_epochs=800 \
     --batch_size=96 \
     --server=lab_center \
     --eval_frequency=5 \
