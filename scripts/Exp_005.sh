cfaea1a2647d3b8baa513a6ac2c1534181ae09ed
theguardsgod <1043843535@qq.com>
2023-12-17 15:16:35 +0800

    lcy-densenet-dense201

--- scripts/Exp_005.sh
+++ scripts/Exp_005.sh
@@ -7,18 +7,20 @@ file_name=`basename $0`
 experiment_index=${file_name##*_}
 experiment_index=${experiment_index%%.*}
 
+
 CUDA_VISIBLE_DEVICES=0 python -u src/trainer.py \
     --experiment_index=$experiment_index \
     --cudas=0 \
-    --n_epochs=1500 \
-    --batch_size=96 \
-    --server=lab_center \
+    --n_epochs=800 \
+    --batch_size=64 \
+    --server=lcy \
     --eval_frequency=5 \
-    --backbone=resnet50 \
-    --learning_rate=1e-1 \
-    --optimizer=SGD \
-    --initialization=pretrained \
+    --backbone=dense201 \
+    --learning_rate=1e-4 \
+    --optimizer=Adam \
+    --initialization=default \
     --num_classes=7 \
     --num_worker=4 \
     --input_channel=3 \
+    --seed 32 \
     2>&1 | tee $log_file
